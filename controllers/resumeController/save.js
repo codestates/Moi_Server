3ab19a2337aeff8f8807db24bbad2bf2cdeff4a1
HyeYeonCha <gpdus335@naver.com>
2021-01-14 15:37:33 +0900

    feature/reasume/save: Edit resume/save Required value exception handling

--- controllers/resumeController/save.js
+++ controllers/resumeController/save.js
@@ -3,6 +3,8 @@ const Resume = require('../../models/resume');
 module.exports = async (req, res) => {
   if (!req.user) res.status(400).json({ save: false });
   try {
+    const { email, name, phone } = req.body.form.info.contact;
+    if (!email || !name || !phone) res.status(400).json({ save: false });
     await Resume.create({
       userId: req.user._id,
       ...req.body,
