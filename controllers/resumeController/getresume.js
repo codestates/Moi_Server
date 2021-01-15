2fb7afebf88b69594b8c5f2223ad22207dc015aa
HyeYeonCha <gpdus335@naver.com>
2021-01-15 11:44:54 +0900

    edit/resume/getresume: delete console.log

--- controllers/resumeController/getresume.js
+++ controllers/resumeController/getresume.js
@@ -5,8 +5,6 @@ module.exports = async (req, res) => {
     if (!req.user) res.status(400).json({ getResume: false });
     const resumeId = req.params.resumeId;
     const resume = await Resume.findOne({ _id: resumeId });
-    console.log('******************');
-    console.log(resume);
     res.status(200).json({ getResume: true, resume: resume });
   } catch (err) {
     console.log(err);
