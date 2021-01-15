dab52c7e6c0c8ad249a396592b84815d72ce5462
HyeYeonCha <gpdus335@naver.com>
2021-01-15 12:21:24 +0900

    edit/resume/edit: edit updateOne

--- controllers/resumeController/edit.js
+++ controllers/resumeController/edit.js
@@ -1,22 +1,23 @@
 const Resume = require('../../models/resume');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   try {
     if (!req.user) res.status(400).json({ edit: false });
-
     const { email, name, phone } = req.body.form.info.contact;
     if (!email || !name || !phone) res.status(400).json({ edit: false });
 
-    const resume = Resume.find({
+    const userResume = await Resume.findOne({
       _id: req.body._id,
-      userId: req.body.userId,
+      userId: req.user._id,
     });
-    if (!resume) res.status(400).json({ edit: false });
-    const editResume = Resume.updateOne(
-      { _id: req.body._id, userId: req.body.userId },
+    if (!userResume) res.status(400).json({ edit: false });
+
+    await Resume.updateOne(
+      { _id: req.body._id, userId: req.user._id },
       { $set: { ...req.body } },
+      { returnNewDocument: true },
     );
-    res.status(200).json({ edit: true, resume: editResume });
+    res.status(200).json({ edit: true });
   } catch (err) {
     console.log(err);
     res.status(400).json({ edit: false });
