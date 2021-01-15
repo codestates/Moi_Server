66784e28bccc8d5014268867b4a165aab4e9cd06
이재홍 <fleuve1008@gmail.com>
2021-01-15 12:19:00 +0900

    feature/resume/deleteResume : Add function to delete resume

--- controllers/resumeController/delete.js
+++ controllers/resumeController/delete.js
@@ -1,1 +1,12 @@
-module.exports = (req, res, next) => {};
+const Resume = require('../../models/resume');
+
+module.exports = async (req, res, next) => {
+  const { resumeId } = req.body;
+  const { _id } = req.user;
+  try {
+    await Resume.deleteOne({ _id: resumeId, userId: _id });
+    res.status(200).json({ isDeleted: true });
+  } catch (err) {
+    next(err);
+  }
+};
