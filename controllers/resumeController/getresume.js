1c84bc0afe2c6f6e5d5587beb2671eac10fb03c9
HyeYeonCha <gpdus335@naver.com>
2021-01-15 11:04:38 +0900

    feature/resume/getresume: Edit res.json

--- controllers/resumeController/getresume.js
+++ controllers/resumeController/getresume.js
@@ -2,14 +2,14 @@ const Resume = require('../../models/resume');
 
 module.exports = (req, res) => {
   try {
-    if (!req.user) res.status(400).json({ edit: false });
+    if (!req.user) res.status(400).json({ getResume: false });
     const resumeId = req.params.resumeId;
     const resume = Resume.findOne({
       query: { _id: resumeId },
     });
-    res.status(200).json({ edit: true, resume: resume });
+    res.status(200).json({ getResume: true, resume: resume });
   } catch (err) {
     console.log(err);
-    res.status(400).json({ edit: false });
+    res.status(400).json({ getResume: false });
   }
 };
