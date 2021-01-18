91d80586dff0aa76106b7949f4daacba62a9a320
HyeYeonCha <gpdus335@naver.com>
2021-01-18 13:34:21 +0900

    edit/resume/getresume: edit async await

--- controllers/resumeController/getresume.js
+++ controllers/resumeController/getresume.js
@@ -1,10 +1,10 @@
 const Resume = require('../../models/resume');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     if (!req.user) res.status(400).json({ getResume: false });
     const resumeId = req.params.resumeId;
-    const resume = Resume.findOne({
+    const resume = await Resume.findOne({
       query: { _id: resumeId },
     });
     res.status(200).json({ getResume: true, resume: resume });
