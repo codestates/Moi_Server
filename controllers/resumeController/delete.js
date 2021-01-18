94c43df1da51ee9c4343c0b6af9a694d9835e265
이재홍 <fleuve1008@gmail.com>
2021-01-19 04:55:26 +0900

    feature/project/Refactoring : Modified for deploy environment

--- controllers/resumeController/delete.js
+++ controllers/resumeController/delete.js
@@ -1,10 +1,15 @@
 const Resume = require('../../models/resume');
+const { deleteProfiles } = require('../../lib/multerMiddleware');
 
 module.exports = async (req, res, next) => {
   const { resumeId } = req.body;
   const { _id } = req.user;
   try {
     if (req.user) {
+      const {
+        form: { avatar },
+      } = await Resume.findOne({ _id: resumeId });
+      await deleteProfiles(avatar);
       await Resume.deleteOne({ _id: resumeId, userId: _id });
       res.status(200).json({ isDeleted: true });
     } else {
--- controllers/resumeController/edit.js
+++ controllers/resumeController/edit.js
@@ -7,23 +7,24 @@ module.exports = async (req, res, next) => {
     if (!email || !name || !phone) res.status(400).json({ isEdited: false });
 
     const userResume = await Resume.findOne({
-      _id: req.body._id,
+      _id: req.body.resumeId,
       userId: req.user._id,
     });
 
     if (!userResume) res.status(400).json({ isEdited: false });
-    const updatedAt = new Date();
+    const resume = new Resume();
+    const updatedAt = resume.dateFormatting(new Date());
     await Resume.updateOne(
-      { _id: req.body._id, userId: req.user._id },
-      { $set: { ...req.body, ...updatedAt } },
+      { _id: req.body.resumeId, userId: req.user._id },
+      { $set: { ...req.body, updatedAt } },
       { returnNewDocument: true },
     );
     const newResume = await Resume.findOne({
-      _id: req.body._id,
+      _id: req.body.resumeId,
       userId: req.user._id,
-    })
+    });
     res.status(200).json({ isEdited: true, newResume });
   } catch (err) {
-    next(err)
+    next(err);
   }
 };
