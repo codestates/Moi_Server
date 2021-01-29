b5c1b3a1cfafe264a2043190b08952fc7b67eca7
이재홍 <fleuve1008@gmail.com>
2021-01-29 11:13:29 +0900

    feature/resume/resume_edit : Modified typo

--- controllers/resumeController/edit.js
+++ controllers/resumeController/edit.js
@@ -8,7 +8,7 @@ module.exports = async (req, res, next) => {
       res.status(400).json({ isEdited: false });
 
     const userResume = await Resume.findOne({
-      _id: req.body.resumeId,
+      _id: req.body.values.resumeId,
       userId: req.user._id,
     });
 
@@ -16,12 +16,12 @@ module.exports = async (req, res, next) => {
     const resume = new Resume();
     const updatedAt = resume.dateFormatting(new Date());
     await Resume.updateOne(
-      { _id: req.body.resumeId, userId: req.user._id },
-      { $set: { ...req.body, updatedAt } },
+      { _id: req.body.values.resumeId, userId: req.user._id },
+      { $set: { ...req.body.values, updatedAt } },
       { returnNewDocument: true },
     );
     const newResume = await Resume.findOne({
-      _id: req.body.resumeId,
+      _id: req.body.values.resumeId,
       userId: req.user._id,
     });
     res.status(200).json({ isEdited: true, newResume });
